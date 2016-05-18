f4476472f3d21c920c08e5b0f2adefa0786ec05d
Miklós Prisznyák <prisznyak.miklos@multiracio.hu>
2016-05-18 18:03:44 +0200

    no logging for events

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ VERSION = "0.3"
 def main():
     image_process()
     window = Window(width=800, height=600, caption='My own cute Pyglet v%s' % VERSION, resizable=True)
-    window.logevents()
+    #window.logevents()
     # Hide the mouse cursor and prevent the mouse from leaving the window.
     window.set_exclusive_mouse(True)
     window.setup()
