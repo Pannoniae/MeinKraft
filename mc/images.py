2d19b8557c19de1a56e2479510a9db5b365bc9a4
Pannoniae <prisznyak.zsombor@gmail.com>
2016-05-18 17:55:29 +0200

    - fixed view.py CTDOS (line 418, return block)

--- mc/images.py
+++ mc/images.py
@@ -1,6 +1,11 @@
 from PIL import Image
 from .constants import *
 
+def import_coords(x, y):
+    # Converting image_process importing values to tex_coords methods.
+    x = x * 64
+    y = y * 64
+    return x, y
 def image_process():
     #Process the image files to texture.png at every start.
     dirt = Image.open(TEXTURE_PATH_DIRT)
@@ -10,11 +15,12 @@ def image_process():
     brick = Image.open(TEXTURE_PATH_BRICK)
     stone = Image.open(TEXTURE_PATH_STONE)
     texture = Image.new('RGB', (256, 256))
-        
-    texture.paste(dirt, (0, 128))
-    texture.paste(sand, (64, 128))
-    texture.paste(stone, (128, 128))
-    texture.paste(grass_side, (0, 192))
-    texture.paste(grass_top, (64, 192))
-    texture.paste(brick, (128, 192))
+
+    texture.paste(dirt, import_coords(0, 2))
+    texture.paste(sand, import_coords(1, 2))
+    texture.paste(stone, import_coords(2, 2))
+    texture.paste(grass_side, import_coords(0, 3))
+    texture.paste(grass_top, import_coords(1, 3))
+    texture.paste(brick, import_coords(2, 3))
+    texture.transpose(PIL.Image.FLIP_TOP_BOTTOM)
     texture.save('texture.png')
