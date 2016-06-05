c4f6fb85d96bc32081837a1dbf34ea8b3e2c0d86
Pannoniae <prisznyak.zsombor@gmail.com>
2016-06-05 12:28:15 +0200

    - trying to create epydoc by removing unicode chars

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# Copyright © 2016 Prisznyák Zsombor <@Pannoniae>
+# Copyright 2016 Prisznyak Zsombor <@Pannoniae>
 # This work is free. You can redistribute it and/or modify it under the
 # terms of the Do What The Fuck You Want To Public License, Version 2b,
 # as published by Sam Hocevar and modified by me. See the LICENSE file for more details.
